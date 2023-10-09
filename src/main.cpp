3b2d109d626d04827a0be3db6e3811718afdaa34
Hiu0302 <hieubom0302@gmail.com>
2023-10-09 16:49:40 +0700

    Testing LED for master & middle with square button

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,28 @@
 #include <Arduino.h>
 #include "../lib/ps4_control/ps4_control.h"
 #include <PS4Controller.h>
+#include <HardwareSerial.h>
+
+HardwareSerial SerialPort(2); // use UART2
 
 void setup(){
   // Start serial with platform baudrate/monitor speed
   Serial.begin(115200);
   init_ps4(); // Initialize PS4 Controller
-  PS4.begin();
-  Serial.println("Ready.");
+
+  // Initiate UART2 for Master EPS32
+  SerialPort.begin(115200, SERIAL_8N1, 16, 17);
 }
 
 void loop() {
   checkInput(); // check all the input from User
+  if (PS4.Square()) // Testing for Square button
+  {
+    SerialPort.print(1);
+  }
+  else
+  {
+    SerialPort.print(0);
+  }
 }
 
