825b53b441726c8b56467cf67cee33cb6a98a644
LongDo1902 <dobaolong2002@gmail.com>
2023-10-19 15:16:04 +0700

    Master update

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ HardwareSerial SerialPort1(1);
 void setup()
 {
   // Start serial with platform baudrate/monitor speed
-  Serial.begin (115200);
-  SerialPort1.begin(115200, SERIAL_8N1, 4, 2);
+  Serial.begin(115200);
+  // SerialPort1.begin(115200, SERIAL_8N1, 4, 2);
  
  // Initiate UART2 for Master EPS32
   SerialPort2.begin(115200, SERIAL_8N1, 16, 17);
@@ -18,16 +18,16 @@ void setup()
 }
 
 void loop() {
-  if(PS4.LStickX()){
-    Serial.printf("Left Stick x at %d\n", PS4.LStickX());
-    SerialPort1.write(PS4.LStickX());
-  }
-
+  // if(PS4.LStickX()){
+  //   Serial.printf("Left Stick x at %d\n", PS4.LStickX());
+  //   SerialPort1.write(PS4.LStickX());
+  // }
+  delay(20);
   if (PS4.Square()){ //increase throttle
    Serial.println("Square Button");
     SerialPort2.write(1); 
   }
-  delay(10);
+  delay(20);
   if (PS4.Cross()){ //decrease throttle
   Serial.println("Cross Button");
     SerialPort2.write(2);
